1fcda81e0f173b2ce31474993bbb45525a2f146d
Ulugbek2360 <ulukbekmakamzonov@gmail.com>
2025-01-31 16:08:03 +0600

    sdf

---
+++ bmi.py
@@ -0,0 +1,17 @@
+heigt=int(input("enter you height:"))
+weigt=int(input("enter you weight:"))
+
+if bmi=weight/ (height/100) ** 2
+
+if bmi <18.5:
+    print("underweight")
+elif 18.5 <=bmi <25:
+    print("overweight")    
+elif 25 <=bmi <30:
+    print("overweight")    
+elif 30 <= <35:
+    print("obesity class 1")    
+elif 35 <=bmi < 39.9:
+    print("obesity class 2")    
+else:
+    print("super fat")    
